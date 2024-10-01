a4fa0429866e6aa66e706bc7ffe184ed6c2a7904
faker <tatsu.ino1202@gmail.com>
2024-10-01 15:28:47 +0900

    最新2

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,14 +1,30 @@
-import { Button} from '@chakra-ui/react'
 import { MdBuild , MdCall } from "react-icons/md"
 import { Stack, VStack } from '@chakra-ui/react'
+import { Button, Text } from "@chakra-ui/react";
 import axios from "axios";
+import { useState } from "react";
+import {
+  IconButton,
+  Spinner,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useEffect} from "react";
 
 export default function Users() {
+    const [user, setUser] = useState<User[]>([]);
 
     async function getItem() {
         try {
           const url = "http://localhost:8000/users";
           const res = await axios.get(url);
+          setUser(res.data);
           console.log(res.data) // response data
           console.log(res.status) // status code
         } catch (err) {
@@ -16,18 +32,43 @@ export default function Users() {
         }
       }
 
+    useEffect(() => {
+      getItem();
+    }, []);
+
   return (
+<>
 
     <Stack direction='row' spacing={4}>
     <Button onClick={getItem} leftIcon={<MdBuild />} colorScheme='pink' variant='solid'>
         Settings
     </Button>
-    <Button onClick={getItem} rightIcon={<MdCall />} colorScheme='blue' variant='outline'>
-        Call us 
-    </Button>
     </Stack>
     
-
+    <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Role</Th>
+                <Th>Color</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {user.map((res) => (
+                <Tr key={res.id}>
+                  <Td>{res.id}</Td>
+                  <Td>{res.name}</Td>
+                  <Td>{res.role}</Td>
+                  <Td>{res.color}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+</>
     )
     
 }
